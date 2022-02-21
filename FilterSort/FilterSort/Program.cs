81f97b2618b863fe55bdb7e7e1d039601293fb76
milos09mihajlovic@gmail.com <milos09mihajlovic@gmail.com>
2022-02-21 17:49:23 +0100

    Implemented SRP

--- FilterSort/FilterSort/Program.cs
+++ FilterSort/FilterSort/Program.cs
@@ -1,57 +1,7 @@
-﻿Console.WriteLine("insert word");
-string input = Console.ReadLine().ToLower();
-Console.WriteLine("choose a method (A/B) : ");
-string chosenMethod = Console.ReadLine().ToLower();
-if (chosenMethod == "a")
-{
-    methodA(input);
-}
-else if (chosenMethod == "b")
-{
-    methodB(input);
-}
-else
-{
-    Console.WriteLine("You didn't choose the method well!");
-}
+﻿using FilterSort;
 
-static void methodA(string word)
-{
-    string res = "";
-    int counter = 0;
-    for (int i = 0; i < word.Length; i++)
-    {
-        string c = word[i].ToString();
-        if (!res.Contains(c))
-        {
-            res = res + c;
-            for (int j = 0; j < word.Length; j++)
-            {
-                if (word[j] == Convert.ToChar(c))
-                {
-                    counter++;
-                }
-            }
-        }
-        else
-        {
-            continue;
-        }
-        res = res + counter;
-        counter = 0;
-    }
-    Console.WriteLine($"{res}");
-}
-static void methodB(string word)
-{
-    string res = "";
-    for (int i = 0; i < word.Length; i++)
-    {
-        string c = word[i].ToString();
-        if (!res.Contains(c))
-        {
-            res = res + c;
-        }
-    }
-    Console.WriteLine($"{res}");
-}
+string input = new UserInput().AskForWord();
+string chosenMethod = new UserInput().AskForMethod();
+
+string result = new StringChecker().GetResult(chosenMethod, input);
+Console.WriteLine(result);
